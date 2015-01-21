ef2036237e90694f84a289924be14eed7d005644
Justin Poehnelt <justin.poehnelt@gmail.com>
2015-01-21 09:14:08 -0700

    added fake data for tour events and members. see the tour template for how to use in django

--- app/views.py
+++ app/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
+from app.models import members as band_members, events
 
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'members': band_members, 'events': events})
+
 
 def members(request):
-    return render(request, 'members.html')
+    return render(request, 'members.html', {'members': band_members})
+
 
 def discography(request):
     return render(request, 'discography.html')
 
+
 def tour(request):
-    return render(request, 'tour.html')
+    return render(request, 'tour.html', {'events': events})
 
