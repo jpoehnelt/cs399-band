23b24ea8f47f27c31f08155759555de69a7a8b88
Justin Poehnelt <justin.poehnelt@gmail.com>
2015-01-21 16:53:10 -0700

    add test for broken urls

--- app/tests.py
+++ app/tests.py
@@ -4,6 +4,9 @@ from django.core.urlresolvers import reverse
 from django.contrib.staticfiles import finders
 from app.settings import STATICFILES_DIRS, BASE_DIR
 from app.views import home, discography, tour, members
+from app.urls import urlpatterns
+from bs4 import BeautifulSoup
+import requests
 
 
 class TestViews(TestCase):
@@ -83,4 +86,39 @@ class TestViews(TestCase):
         self.assertContains(response, reverse('discography'))
         self.assertContains(response, reverse('home'))
         self.assertContains(response, reverse('tour'))
+
+    def test_broken_links(self):
+        def check_link(href):
+            if "//" in href:
+                self.assertEqual(requests.get(href).status_code, 200)
+            elif '/static/' in href:
+                # remove static for django
+                static = href.replace('/static/', '')
+                self.assertIsNotNone(finders.find(static))
+            else:
+                self.assertEqual(self.client.get(href).status_code, 200)
+
+        for pattern in urlpatterns:
+            url = reverse(pattern.name)
+            response = self.client.get(url)
+            soup = BeautifulSoup(str(response))
+            a = soup.find_all('a')
+            for link in a:
+                # get link address
+                href = link.get('href')
+
+                if href is None or len(href) == 0 or href[0] == '#':
+                    continue
+
+                check_link(href)
+
+            img = soup.find_all('img')
+            for link in img:
+                href = link.get('src')
+                if href is None or len(href) == 0 or href[0] == '#':
+                    continue
+
+                check_link(href)
+
+
 
